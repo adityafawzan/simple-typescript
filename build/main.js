58c7721a48f1c5f83f0edb79fd712ef380e2ea3c
adityafawzan <fawzan89@gmail.com>
2022-07-27 22:49:07 +0700

    init

---
+++ build/main.js
@@ -0,0 +1,5 @@
+"use strict";
+const hello = (name) => {
+    return `hello ${name}`;
+};
+console.log(hello("adit"));
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+const hello = (name: string): string => {
+  return `hello ${name}`;
+};
+
+console.log(hello("adit"));
